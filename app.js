f4decdae5572c6ea8e389470e05fbaa0d5c4e450
Rohit Kumar <07nith.me@gmail.com>
2023-09-28 12:06:15 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {}, "Hello world from React!");
+            
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(heading);
